bcd0bd90c3e21fbc68983db7e239e8cc0455fd92
Lukáš Bašista <xbasista@fi.muni.cz>
2020-03-07 21:43:11 +0100

    HW01 - edit 1

--- hw01/ascii85.c
+++ hw01/ascii85.c
@@ -3,40 +3,42 @@
 #include <string.h>
 #include <ctype.h>
 
-void printEncoded(unsigned int n){
+void printEncoded(unsigned int n) {
     int c;
     int a[5];
-    for (int i = 0; i <= 4; i++){
+    for (int i = 0; i <= 4; i++) {
         c = n % 85 + 33;
         n = n / 85;
-        a[i]=c;
+        a[i] = c;
     }
-    for (int i = 4; i >= 0; i--){
+    for (int i = 4; i >= 0; i--) {
         putchar(a[i]);
     }
 }
 
-void printDecoded(unsigned int n){
+void printDecoded(unsigned int n) {
     unsigned int c = 255;
     int a[4];
-    for (int i = 0; i < 4; i++){
+    for (int i = 0; i < 4; i++) {
         a[i] = n & c;
         n = n >> 8;
     }
-    for (int i = 3; i >= 0; i--){
+    for (int i = 3; i >= 0; i--) {
         putchar(a[i]);
     }
 }
 
-int encode(void)
-{
+int encode(void) {
     int c;
     int cCount = 0;
     unsigned int c4 = 0;
     while ((c = getchar()) != EOF) {
         cCount++;
         c4 = c4 | c;
-        if (cCount%4!=0) {
+        if (c <= 33 || c >= 117) {
+            return 1;
+        }
+        if (cCount % 4 != 0) {
             c4 = c4 << 8;
         }
         if (cCount % 4 == 0) {
@@ -44,26 +46,29 @@ int encode(void)
             c4 = 0;
         }
     }
-    if (cCount%4!=0){
-        int a=4-cCount%4;
-        for (int i=1; i<a+1; i++){
+    if (cCount % 4 != 0) {
+        int a = 4 - cCount % 4;
+        for (int i = 1; i < a + 1; i++) {
             c4 = c4 | '\0';
-            if ((cCount+i)%4!=0){ c4 = c4<<8; }
+            if ((cCount + i) % 4 != 0) { c4 = c4 << 8; }
         }
         printEncoded(c4);
+    }
+    if (cCount % 5 != 0) {
+        return 1;
     }
     printf("\n");
     return 0;
 }
 
-int decode(void)
-{
+int decode(void) {
     int c;
     int cCount = 0;
     unsigned int c4 = 0;
     while ((c = getchar()) != EOF) {
         if (isspace(c)) {
-            continue;} else {
+            continue;
+        } else {
             cCount++;
             c4 = c4 * 85 + (c - 33);
             if (cCount % 5 == 0) {
@@ -78,8 +83,7 @@ int decode(void)
 // ================================
 // DO NOT MODIFY THE FOLLOWING CODE
 // ================================
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     int retcode = 1;
 
     if (argc == 1 || (argc == 2 && strcmp(argv[1], "-e") == 0)) {
