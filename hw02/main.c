3423e274274db069728f9daf0337b948f70d5ac5
Lukáš Bašista <xbasista@fi.muni.cz>
2020-03-31 21:15:40 +0200

    HW02 - edit 1

--- hw02/main.c
+++ hw02/main.c
@@ -30,7 +30,8 @@ char most_color(const char colors[]);
 
 int straightflush(int val[], char col[]);
 
-int main(void) {
+int main(void)
+{
     int values[CARDS_HAND][CARDS];
     char colors[CARDS_HAND][CARDS];
     while (loadCards(values, colors) != 0) {
@@ -40,14 +41,16 @@ int main(void) {
     }
 }
 
-int loadCards(int val[CARDS_HAND][CARDS], char col[CARDS_HAND][CARDS]) {
+int loadCards(int val[CARDS_HAND][CARDS], char col[CARDS_HAND][CARDS])
+{
     char d;
     char c;
     char n;
     int card = 0;
 
     for (int i = 0; i < 9; ++i) {
-        if (scanf(" %1c%1c%c", &d, &c, &n) == EOF) return 0;
+        if (scanf(" %1c%1c%c", &d, &c, &n) == EOF)
+            return 0;
         if (c != 'h' && c != 'd' && c != 's' && c != 'c') {
             fprintf(stderr, "%c: invalid color\n", c);
             return 0;
@@ -61,12 +64,18 @@ int loadCards(int val[CARDS_HAND][CARDS], char col[CARDS_HAND][CARDS]) {
             fprintf(stderr, "Missing white space\n");
             return 0;
         }
-        if (d >= '2' && d <= '9') card = d - '0';
-        else if (d == 'T') card = 10;
-        else if (d == 'J') card = 11;
-        else if (d == 'Q') card = 12;
-        else if (d == 'K') card = 13;
-        else if (d == 'A') card = 14;
+        if (d >= '2' && d <= '9')
+            card = d - '0';
+        else if (d == 'T')
+            card = 10;
+        else if (d == 'J')
+            card = 11;
+        else if (d == 'Q')
+            card = 12;
+        else if (d == 'K')
+            card = 13;
+        else if (d == 'A')
+            card = 14;
         else {
             fprintf(stderr, "%c: Invalid value\n", d);
             return 0;
@@ -91,14 +100,18 @@ int loadCards(int val[CARDS_HAND][CARDS], char col[CARDS_HAND][CARDS]) {
     return 1;
 }
 
-char match_result(int val1[], char col1[], int val2[], char col2[]) {
+char match_result(int val1[], char col1[], int val2[], char col2[])
+{
     int p1 = straightflush(val1, col1);
     int p2 = straightflush(val2, col2);
 
     if (p1 != 0 || p2 != 0) {
-        if (p1 == p2) return 'D';
-        if (p1 > p2) return 'W';
-        else return 'L';
+        if (p1 == p2)
+            return 'D';
+        if (p1 > p2)
+            return 'W';
+        else
+            return 'L';
     }
 
     p1 = four_of_kind(val1);
@@ -108,8 +121,10 @@ char match_result(int val1[], char col1[], int val2[], char col2[]) {
         if (p1 == p2) {
             return highCard(val1, val2, 1, p1, 0);
         }
-        if (p1 > p2) return 'W';
-        else return 'L';
+        if (p1 > p2)
+            return 'W';
+        else
+            return 'L';
     }
 
     int tok1 = three_of_kind(val1);
@@ -119,13 +134,18 @@ char match_result(int val1[], char col1[], int val2[], char col2[]) {
 
     if ((tok1 != 0 && pair1 != 0) || (tok2 != 0 && pair2 != 0)) {
         if (tok1 == tok2) {
-            if (pair1 == pair2) return 'D';
-            if (pair1 > pair2) return 'W';
-            else return 'L';
+            if (pair1 == pair2)
+                return 'D';
+            if (pair1 > pair2)
+                return 'W';
+            else
+                return 'L';
 
         }
-        if (tok1 > tok2) return 'W';
-        else return 'L';
+        if (tok1 > tok2)
+            return 'W';
+        else
+            return 'L';
     }
 
     char flush1 = flush(col1);
@@ -134,50 +154,70 @@ char match_result(int val1[], char col1[], int val2[], char col2[]) {
     if (flush1 != 'n' && flush2 != 'n') {
         return highCard_color(val1, val2, col1, col2, flush1);
     }
-    if (flush1 != 'n') return 'W';
-    if (flush2 != 'n') return 'L';
+    if (flush1 != 'n')
+        return 'W';
+    if (flush2 != 'n')
+        return 'L';
 
     p1 = straight(val1);
     p2 = straight(val2);
 
     if (p1 != 0 || p2 != 0) {
-        if (p1 == p2) return 'D';
-        if (p1 > p2) return 'W';
-        else return 'L';
+        if (p1 == p2)
+            return 'D';
+        if (p1 > p2)
+            return 'W';
+        else
+            return 'L';
     }
 
     if (tok1 != 0 && tok2 != 0) {
         if (tok1 == tok2) {
             return highCard(val1, val2, 2, tok1, 0);
         }
-        if (tok1 > tok2) return 'W';
-        else return 'L';
+        if (tok1 > tok2)
+            return 'W';
+        else
+            return 'L';
     }
-    if (tok1 != 0) return 'W';
-    if (tok2 != 0) return 'L';
+    if (tok1 != 0)
+        return 'W';
+    if (tok2 != 0)
+        return 'L';
 
 
     if (pair1 != 0 && pair2 != 0) {
         int spair1 = pair(val1, pair1);
         int spair2 = pair(val2, pair2);
         if (spair1 == 0 && spair2 == 0) {
-            if (pair1 == pair2) return highCard(val1, val2, 3, pair1, spair1);
-            if (pair1 > pair2) return 'W';
-            else return 'L';
+            if (pair1 == pair2)
+                return highCard(val1, val2, 3, pair1, spair1);
+            if (pair1 > pair2)
+                return 'W';
+            else
+                return 'L';
         }
         if (spair1 != 0 && spair2 != 0) {
 
-            if (pair1 == pair2 && spair1 == spair2) return highCard(val1, val2, 1, pair1, spair1);
-            if (pair1 == pair2 && spair1 > spair2) return 'W';
-            if (pair1 == pair2 && spair1 < spair2) return 'L';
-            if (pair1 > pair2) return 'W';
-            else return 'L';
+            if (pair1 == pair2 && spair1 == spair2)
+                return highCard(val1, val2, 1, pair1, spair1);
+            if (pair1 == pair2 && spair1 > spair2)
+                return 'W';
+            if (pair1 == pair2 && spair1 < spair2)
+                return 'L';
+            if (pair1 > pair2)
+                return 'W';
+            else
+                return 'L';
         }
-        if (spair1 != 0) return 'W';
+        if (spair1 != 0)
+            return 'W';
         return 'L';
     }
-    if (pair1 != 0) return 'W';
-    if (pair2 != 0) return 'L';
+    if (pair1 != 0)
+        return 'W';
+    if (pair2 != 0)
+        return 'L';
 
     return highCard(val1, val2, 5, 0, 0);
 
@@ -185,16 +225,19 @@ char match_result(int val1[], char col1[], int val2[], char col2[]) {
 }
 
 
-int duplicates(int val[CARDS], char col[CARDS]) {
+int duplicates(int val[CARDS], char col[CARDS])
+{
     for (int i = 0; i < CARDS; ++i) {
         for (int j = i + 1; j < CARDS; ++j) {
-            if (val[i] == val[j] && col[i] == col[j]) return 1;
+            if (val[i] == val[j] && col[i] == col[j])
+                return 1;
         }
     }
     return 0;
 }
 
-void sort(int val[], char col[]) {
+void sort(int val[], char col[])
+{
     for (int i = 0; i < CARDS - 1; ++i) {
         for (int j = 0; j < CARDS - 1 - i; ++j) {
             if (val[j] > val[j + 1]) {
@@ -209,21 +252,23 @@ void sort(int val[], char col[]) {
     }
 }
 
-char highCard(const int val1[], const int val2[], int n, int ignore, int ignore2) {
+char highCard(const int val1[], const int val2[], int n, int ignore, int ignore2)
+{
     int i1 = CARDS - 1;
     int i2 = CARDS - 1;
     int counter = 0;
     while (counter < n && i1 >= 0 && i2 >= 0) {
         if (val1[i1] == ignore || val2[i2] == ignore || val1[i1] == ignore2 || val2[i2] == ignore2) {
-            if (val1[i1] == ignore || val1[i1] == ignore2) i1--;
-            if (val2[i2] == ignore || val2[i2] == ignore2) i2--;
-
+            if (val1[i1] == ignore || val1[i1] == ignore2)
+                i1--;
+            if (val2[i2] == ignore || val2[i2] == ignore2)
+                i2--;
             continue;
         }
-        if (val1[i1] > val2[i2]) return 'W';
-        if (val1[i1] < val2[i2]) {
+        if (val1[i1] > val2[i2])
+            return 'W';
+        if (val1[i1] < val2[i2])
             return 'L';
-        }
         counter++;
         i1--;
         i2--;
@@ -231,107 +276,139 @@ char highCard(const int val1[], const int val2[], int n, int ignore, int ignore2
     return 'D';
 }
 
-char highCard_color(const int val1[], const int val2[], char col1[], char col2[], char c) {
+char highCard_color(const int val1[], const int val2[], char col1[], char col2[], char c)
+{
     int i1 = CARDS - 1;
     int i2 = CARDS - 1;
     int counter = 0;
     while (counter < 5 && i1 >= 0 && i2 >= 0) {
         if (col1[i1] == c && col2[i2] == c) {
-            if (val1[i1] > val2[i2]) return 'W';
-            if (val1[i1] < val2[i2]) return 'L';
-            else counter++;
+            if (val1[i1] > val2[i2])
+                return 'W';
+            if (val1[i1] < val2[i2])
+                return 'L';
+            else
+                counter++;
             i1--;
             i2--;
         }
-        if (col1[i1] != c) i1--;
-        if (col2[i2] != c) i2--;
+        if (col1[i1] != c)
+            i1--;
+        if (col2[i2] != c)
+            i2--;
     }
     return 'D';
 }
 
-int four_of_kind(int val[]) {
+int four_of_kind(int val[])
+{
     for (int i = CARDS - 1; i > 2; --i) {
-        if (val[i] == val[i - 1] && val[i] == val[i - 2] && val[i] == val[i - 3]) return val[i];
+        if (val[i] == val[i - 1] && val[i] == val[i - 2] && val[i] == val[i - 3])
+            return val[i];
     }
     return 0;
 }
 
-int three_of_kind(int val[]) {
+int three_of_kind(int val[])
+{
     for (int i = CARDS - 1; i > 1; --i) {
-        if (val[i] == val[i - 1] && val[i] == val[i - 2]) return val[i];
+        if (val[i] == val[i - 1] && val[i] == val[i - 2])
+            return val[i];
     }
     return 0;
 }
 
-int pair(int val[], int ignore) {
+int pair(int val[], int ignore)
+{
     for (int i = CARDS - 1; i > 0; --i) {
-        if (val[i] != ignore && val[i] == val[i - 1]) return val[i];
+        if (val[i] != ignore && val[i] == val[i - 1])
+            return val[i];
     }
     return 0;
 }
 
-int straightflush(int val[], char col[]) {
+int straightflush(int val[], char col[])
+{
     char color = most_color(col);
     int counter = 0;
-    if (val[6] == 14 && val[0] == 2 && col[6] == col[0]) counter++;
+    if (val[6] == 14 && val[0] == 2 && col[6] == col[0])
+        counter++;
     for (int i = 0; i < CARDS - 1; ++i) {
-        if (val[i] == val[i + 1] && col[i] != color) continue;
-        if (val[i] == val[i + 1] - 1 && col[i] == color && col[i + 1] == color) counter++;
+        if (val[i] == val[i + 1] && col[i] != color)
+            continue;
+        if (val[i] == val[i + 1] - 1 && col[i] == color && col[i + 1] == color)
+            counter++;
         else {
-            if (counter >= 4) return val[i];
-            else counter = 0;
+            if (counter >= 4)
+                return val[i];
+            else
+                counter = 0;
         }
     }
-    if (counter >= 4) return val[CARDS];
+    if (counter >= 4)
+        return val[CARDS];
     return 0;
 }
 
-int straight(int val[]) {
+int straight(int val[])
+{
     int counter = 0;
-    if (val[6] == 14 && val[0] == 2) counter++;
+    if (val[6] == 14 && val[0] == 2)
+        counter++;
 
     for (int i = 0; i < CARDS; ++i) {
-        if (val[i] == val[i + 1]) continue;
-        if (val[i] == val[i + 1] - 1) counter++;
+        if (val[i] == val[i + 1])
+            continue;
+        if (val[i] == val[i + 1] - 1)
+            counter++;
         else {
-            if (counter >= 4) return val[i];
+            if (counter >= 4)
+                return val[i];
             counter = 0;
         }
     }
     return 0;
 }
 
-char most_color(const char colors[]) {
-    int counter[4] = {0, 0, 0, 0};
+char most_color(const char colors[])
+{
+    int counter[4] = { 0, 0, 0, 0 };
     for (int i = 0; i < CARDS; ++i) {
-        if (colors[i] == 'h') counter[0]++;
-        if (colors[i] == 'd') counter[1]++;
-        if (colors[i] == 's') counter[2]++;
-        if (colors[i] == 'c') counter[3]++;
+        if (colors[i] == 'h')
+            counter[0]++;
+        if (colors[i] == 'd')
+            counter[1]++;
+        if (colors[i] == 's')
+            counter[2]++;
+        if (colors[i] == 'c')
+            counter[3]++;
     }
     for (int j = 0; j < 4; ++j) {
         if (counter[j] >= 5)
             switch (j) {
-                case 0:
-                    return 'h';
-                case 1:
-                    return 'd';
-                case 2:
-                    return 's';
-                case 3:
-                    return 'c';
+            case 0:
+                return 'h';
+             case 1:
+                return 'd';
+             case 2:
+                return 's';
+             case 3:
+                return 'c';
             }
     }
     return 'n';
 }
 
-char flush(char col[]) {
+char flush(char col[])
+{
     char color = most_color(col);
     if (color != 'n') {
         int counter = 0;
         for (int i = 0; i < CARDS; ++i) {
-            if (col[i] == color) counter++;
-            if (counter >= 5) return color;
+            if (col[i] == color)
+                counter++;
+            if (counter >= 5)
+                return color;
         }
     }
     return 'n';
