44b79c291ba618c04cec5fe855b40455eae502c4
Lukáš Bašista <xbasista@fi.muni.cz>
2020-05-02 21:25:13 +0200

    3

--- hw04/main.c
+++ hw04/main.c
@@ -7,148 +7,43 @@
  *
 */
 
+#include "graph.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <limits.h>
-#include "graph.h"
 #include "heap.h"
 #include "graph-private.h"
+
+int run(char *, char *, char *, char *, char *);
+
+void release(FILE *, FILE *, Graph *, Heap *);
 
 bool loadFile_nodes(FILE *, Graph *);
 
 bool loadFile_edges(FILE *, Graph *);
 
+int countChars(char *, char);
+
 Heap *findPath(Graph *, unsigned int, Node *);
 
 void printPath(unsigned int, Node *, FILE *);
 
-void release(FILE *, FILE *, Graph *, Heap *);
-
-int run(char *, char *, char *, char *, char *);
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     switch (argc) {
-        case 5:
-            return run(argv[1], argv[2], argv[3], argv[4], NULL);
-        case 6:
-            return run(argv[1], argv[2], argv[3], argv[4], argv[5]);
-        default:
-            fprintf(stderr, "Invalid number of parameters [%d].\n", argc - 1);
-            return 1;
+    case 5:
+        return run(argv[1], argv[2], argv[3], argv[4], NULL);
+    case 6:
+        return run(argv[1], argv[2], argv[3], argv[4], argv[5]);
+    default:
+        fprintf(stderr, "Invalid number of parameters [%d].\n", argc - 1);
+        return 1;
     }
 }
 
-int countChars(char *s, char c) {
-    return *s == '\0'
-           ? 0
-           : countChars(s + 1, c) + (*s == c);
-}
-
-bool loadFile_nodes(FILE *file, Graph *graph) {
-    char line[201] = "";
-    for (;;) {
-        if ((fgets(line, 200, file)) == NULL) {
-            break;
-        }
-        if (countChars(line, ',') != 6) {
-            fprintf(stderr, "Invalid number of commas in line.\n");
-            return false;
-        }
-        char *token = strtok(line, ",");
-        if (!graph_insert_node(graph, strtol(token, NULL, 10)))
-            return false;
-    }
-    return true;
-}
-
-bool loadFile_edges(FILE *file, Graph *graph) {
-    unsigned int source = 0;
-    unsigned int dest = 0;
-    int mintime = 0;
-    char line[201] = "";
-    for (;;) {
-        if ((fgets(line, 200, file)) == NULL) {
-            break;
-        }
-        if (countChars(line, ',') != 6) {
-            fprintf(stderr, "Invalid number of commas in line.\n");
-            return false;
-        }
-        char *token = strtok(line, ",");
-        source = strtol(token, NULL, 10);
-        token = strtok(NULL, ",");
-        dest = strtol(token, NULL, 10);
-        strtok(NULL, ",");
-        token = strtok(NULL, ",");
-        mintime = strtol(token, NULL, 10);
-        if (!graph_insert_edge(graph, source, dest, mintime))
-            return false;
-    }
-    return true;
-}
-
-Heap *findPath(Graph *graph, unsigned int id, Node *dest) {
-    Heap *heap = heap_new_from_graph(graph);
-    if (!heap) {
-        fprintf(stderr, "Cannot allocate new memory.\n");
-        return NULL;
-    }
-    Node *source_node = graph_get_node(graph, id);
-    if (!source_node) {
-        heap_free(heap);
-        fprintf(stderr, "Invalid source node id.\n");
-        return NULL;
-    }
-    heap_decrease_distance(heap, source_node, 0, NULL);
-    Node *node = NULL;
-    Node *temp = NULL;
-    unsigned int alt = 0;
-
-    while (!heap_is_empty(heap)) {
-        node = heap_extract_min(heap);
-        alt = node_get_distance(node);
-        struct edge *temp_edges = node_get_edges(node);
-        unsigned int d = alt;
-        if (alt == UINT_MAX) break;
-        for (unsigned short i = 0; i < node_get_n_outgoing(node); i++) {
-            temp = temp_edges[i].destination;
-            alt = d + temp_edges[i].mindelay;
-            if (alt < node_get_distance(temp)) {
-                heap_decrease_distance(heap, temp, alt, node);
-            }
-        }
-        if (node == dest) {
-            return heap;
-        }
-    }
-    return heap;
-}
-
-void printPath(unsigned int id, Node *dest, FILE *output) {
-    fprintf(output, "digraph {\n");
-    if (id != node_get_id(dest)) {
-        Node *temp_node = dest;
-        Node *temp_prev = node_get_previous(temp_node);
-        while (temp_prev) {
-            int distance = node_get_distance(temp_node) - node_get_distance(temp_prev);
-            fprintf(output, "\t%u -> %u [label=%u];\n", node_get_id(temp_prev), node_get_id(temp_node), distance);
-            temp_node = temp_prev;
-            temp_prev = node_get_previous(temp_prev);
-        }
-    }
-    fprintf(output, "}\n");
-}
-
-void release(FILE *nodes, FILE *edges, Graph *graph, Heap *heap) {
-    if (nodes) fclose(nodes);
-    if (edges) fclose(edges);
-    if (graph) graph_free(graph);
-    if (heap) heap_free(heap);
-
-}
-
-int run(char *nodesFile, char *edgesFile, char *sourceNode, char *destNode, char *outputFile) {
+int run(char *nodesFile, char *edgesFile, char *sourceNode, char *destNode, char *outputFile)
+{
     FILE *nodes = NULL;
     FILE *edges = NULL;
 
@@ -208,6 +103,125 @@ int run(char *nodesFile, char *edgesFile, char *sourceNode, char *destNode, char
         fclose(file);
     }
     release(nodes, edges, graph, heap);
-    if (error) return 1;
+    if (error)
+        return 1;
     return 0;
 }
+
+void release(FILE *nodes, FILE *edges, Graph *graph, Heap *heap)
+{
+    if (nodes)
+        fclose(nodes);
+    if (edges)
+        fclose(edges);
+    if (graph)
+        graph_free(graph);
+    if (heap)
+        heap_free(heap);
+}
+
+bool loadFile_nodes(FILE *file, Graph *graph)
+{
+    char line[201] = "";
+    for (;;) {
+        if ((fgets(line, 200, file)) == NULL) {
+            break;
+        }
+        if (countChars(line, ',') != 6) {
+            fprintf(stderr, "Invalid number of commas in line.\n");
+            return false;
+        }
+        char *token = strtok(line, ",");
+        if (!graph_insert_node(graph, strtol(token, NULL, 10)))
+            return false;
+    }
+    return true;
+}
+
+bool loadFile_edges(FILE *file, Graph *graph)
+{
+    unsigned int source = 0;
+    unsigned int dest = 0;
+    int mintime = 0;
+    char line[201] = "";
+    for (;;) {
+        if ((fgets(line, 200, file)) == NULL) {
+            break;
+        }
+        if (countChars(line, ',') != 6) {
+            fprintf(stderr, "Invalid number of commas in line.\n");
+            return false;
+        }
+        char *token = strtok(line, ",");
+        source = strtol(token, NULL, 10);
+        token = strtok(NULL, ",");
+        dest = strtol(token, NULL, 10);
+        strtok(NULL, ",");
+        token = strtok(NULL, ",");
+        mintime = strtol(token, NULL, 10);
+        if (!graph_insert_edge(graph, source, dest, mintime))
+            return false;
+    }
+    return true;
+}
+
+int countChars(char *s, char c)
+{
+    return *s == '\0'? 0 : countChars(s + 1, c) + (*s == c);
+}
+
+Heap *findPath(Graph *graph, unsigned int id, Node *dest)
+{
+    Heap *heap = heap_new_from_graph(graph);
+    if (!heap) {
+        fprintf(stderr, "Cannot allocate new memory.\n");
+        return NULL;
+    }
+    Node *source_node = graph_get_node(graph, id);
+    if (!source_node) {
+        heap_free(heap);
+        fprintf(stderr, "Invalid source node id.\n");
+        return NULL;
+    }
+    heap_decrease_distance(heap, source_node, 0, NULL);
+    Node *node = NULL;
+    Node *temp = NULL;
+    unsigned int alt = 0;
+
+    while (!heap_is_empty(heap)) {
+        node = heap_extract_min(heap);
+        alt = node_get_distance(node);
+        struct edge *temp_edges = node_get_edges(node);
+        unsigned int d = alt;
+        if (alt == UINT_MAX)
+            break;
+        for (unsigned short i = 0; i < node_get_n_outgoing(node); i++) {
+            temp = temp_edges[i].destination;
+            alt = d + temp_edges[i].mindelay;
+            if (alt < node_get_distance(temp)) {
+                heap_decrease_distance(heap, temp, alt, node);
+            }
+        }
+        if (node == dest) {
+            return heap;
+        }
+    }
+    return heap;
+}
+
+void printPath(unsigned int id, Node *dest, FILE *output)
+{
+    fprintf(output, "digraph {\n");
+    if (id != node_get_id(dest)) {
+        Node *temp_node = dest;
+        Node *temp_prev = node_get_previous(temp_node);
+        while (temp_prev) {
+            int distance = node_get_distance(temp_node) - node_get_distance(temp_prev);
+            fprintf(output, "\t%u -> %u [label=%u];\n", node_get_id(temp_prev), node_get_id(temp_node), distance);
+            temp_node = temp_prev;
+            temp_prev = node_get_previous(temp_prev);
+        }
+    }
+    fprintf(output, "}\n");
+}
+
