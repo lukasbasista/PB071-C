4f9de82ff06c250d734bca24f501e0a5b18e5af1
lukasbasista <72585590+lukasbasista@users.noreply.github.com>
2020-10-09 04:31:19 +0200

    added files from the assignment

---
+++ hw04/Files from the assignment/main.c
@@ -0,0 +1,14 @@
+/**
+ * @file    main.c
+ * \mainpage HW04 Documentation
+ *
+ *
+ * On the 'Files' page, there is a list of documented files with brief descriptions.
+ *
+*/
+
+/*
+ * If you want to document it Doxygen, the first comment must remain as
+ * it is now. The comment tells Doxygen that this file should be
+ * processed
+ *  */
